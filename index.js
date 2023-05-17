1760dda62becc065f787bf2a4fb1f3b593c50047
Igor <zhigalovzenit112233@gmail.com>
2023-05-17 21:51:10 +0300

    create server to client

--- index.js
+++ index.js
@@ -1,43 +1,40 @@
-const yargs = require('yargs');
+const http = require('http');
+const chalk = require('chalk');
+const fs = require('fs/promises');
+const path = require('path');
+const { addNote } = require('./notes.controller');
 
-const { addNote, getNotes, printNotes, deleteNote } = require('./notes.controller');
+const port = 3000;
+const basePath = path.join(__dirname, 'pages');
 
-yargs.command({
-  command: 'add',
-  describe: 'add new buying note to',
-  builder: {
-    title: {
-      type: 'string',
-      describe: 'Buying title',
-      demandOption: true,
-    },
-  },
-  async handler({ title }) {
-    await addNote(title);
-  },
+const server = http.createServer(async (request, response) => {
+  if (request.method === 'GET') {
+    const content = await fs.readFile(path.join(basePath, 'index.html'));
+    // response.setHeader('Content-Type', 'text/html');
+    response.writeHead(200, {
+      'Content-Type': 'text/html',
+    });
+    response.end(content);
+  } else if (request.method === 'POST') {
+    const body = [];
+
+    response.writeHead(200, {
+      'Content-Type': 'text/plain; charset=utf-8',
+    });
+
+    request.on('data', (data) => {
+      body.push(Buffer.from(data));
+    });
+
+    request.on('end', () => {
+      const title = body.toString().split('=')[1].replaceAll('+', ' ');
+      addNote(title);
+
+      response.end(`Title = ${title}`);
+    });
+  }
 });
 
-yargs.command({
-  command: 'list',
-  describe: 'print all buying list',
-  async handler() {
-    printNotes();
-  },
+server.listen(port, () => {
+  console.log(chalk.green(`Server has been started on port ${port}...`));
 });
-
-yargs.command({
-  command: 'remove',
-  describe: 'delete note by id',
-  builder: {
-    id: {
-      type: 'string',
-      describe: 'Buying note id',
-      demandOption: true,
-    },
-  },
-  async handler({ id }) {
-    await deleteNote(id);
-  },
-});
-
-yargs.parse();
